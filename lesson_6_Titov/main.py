579af3b305a4f2df046637b22c555c716a940652
PavloTitov <p.p.titov@gmail.com>
2020-06-21 19:34:34 +0300

    second commit

--- lesson_6_Titov/main.py
+++ lesson_6_Titov/main.py
@@ -1,3 +1,31 @@
-for i in range(7,50,7):
+#1 задача
+for i in range(7,30,7):
     print('i=', i) 
- 
+#2 задача
+count=0
+for i in range (0,30):
+    print('i=', i)
+print()
+for i in reversed(range (0,30)):
+    print('i=', i)
+print()
+for i in range(0,30):
+    if i % 7== 0:
+        print('i=', i)
+print()
+for i in range(0,30):
+    if i % 5 ==0:
+        count +=1
+print(count)
+#3 задача
+count=0
+for i in range(0,50):
+    if i%7==0 and i%5==0:
+        print('FizzBuzz')
+    elif i%3==0:
+        print('Fizz')
+    elif i%5==0:
+        print('Buzz')
+    else:
+        print(i)
+
